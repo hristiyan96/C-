02749371894433c81c6a8d3d5040e641efcd1c9d
Hristiyan Stefanov Velev <145477797+hristiyan96@users.noreply.github.com>
2024-02-28 17:32:23 +0200

    Add files via upload

---
+++ 04.Back in 30 minutes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int hh = int.Parse(Console.ReadLine());
+        int mm = int.Parse(Console.ReadLine());
+
+        mm += 30;
+       
+        if(mm > 60)
+        {
+            hh ++;
+            mm = mm - 60;
+
+        }
+         if(hh >= 24)
+        {
+           hh = hh - 24;
+        }
+         if(mm < 10)
+        {
+            Console.WriteLine($"{hh}:{mm:d2}");
+            
+        }
+         else if(mm >= 10)
+        {
+            Console.WriteLine($"{hh}:{mm}");
+        }
+          
+    }
+}
---
+++ 06.Foreign Languages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string countryName = Console.ReadLine();
+        switch(countryName)
+        {
+            case "England":
+            case "USA":
+                Console.WriteLine("English");
+                break;
+            case "Spain":
+            case "Mexico":
+            case "Argentina":
+                Console.WriteLine("Spanish");
+                break;
+            default:
+                Console.WriteLine("unknown");
+                break;
+
+        }
+    }
+}
